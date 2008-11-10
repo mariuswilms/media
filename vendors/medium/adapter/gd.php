91784865d00b6266c425adeeb9cc95a04466a2d9
David Persson <davidpersson@qeweurope.org>
2008-11-10 23:17:37 +0100

    Fixing assignment of resulting resource in GdMediumAdapter::crop

--- vendors/medium/adapter/gd.php
+++ vendors/medium/adapter/gd.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Gd Medium Adapter File
- * 
+ *
  * Copyright (c) $CopyrightYear$ David Persson
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,10 +21,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE
- * 
+ *
  * PHP version $PHPVersion$
  * CakePHP version $CakePHPVersion$
- * 
+ *
  * @category   media handling
  * @package    attm
  * @subpackage attm.plugins.media.libs.medium.adapter
@@ -35,13 +35,13 @@
  * @version    Release: $Version$
  * @link       http://cakeforge.org/projects/attm The attm Project
  * @since      media plugin 0.50
- * 
+ *
  * @modifiedby   $LastChangedBy$
  * @lastmodified $Date$
  */
 /**
  * Gd Medium Adapter Class
- * 
+ *
  * @category   media handling
  * @package    attm
  * @subpackage attm.plugins.media.libs.medium.adapter
@@ -57,13 +57,13 @@ class GdMediumAdapter extends MediumAdapter {
 							);
 	var $_Image;
 	var $_formatMap = array(
-						'image/jpeg' => 'jpeg', 
-						'image/gif' => 'gif', 
+						'image/jpeg' => 'jpeg',
+						'image/gif' => 'gif',
 						'image/png' => 'png',
 						'image/gd' => 'gd',
 						'image/vnd.wap.wbmp' => 'wbmp',
 						'image/xbm' => 'xbm',
-						);	
+						);
 	var $_format;
 /**
  * Enter description here...
@@ -74,42 +74,42 @@ class GdMediumAdapter extends MediumAdapter {
 /**
  * The compression level used for PNGs
  *
- * Defaults to 0 because a higher value causes 
+ * Defaults to 0 because a higher value causes
  * errors in some environments
- * 
+ *
  * @var integer A value between 0-9 where 9 is highest compression
  */
 	var $_pngCompression = null;
 /**
  * Enter description here...
- * 
+ *
  * PNG_NO_FILTER or PNG_ALL_FILTERS
  * PNG_FILTER_NONE, PNG_FILTER_SUB, PNG_FILTER_UP, PNG_FILTER_AVG, PNG_FILTER_PAETH
  *
  * @var unknown_type
  */
 	var $_pngFilters = null;
-	
+
 	function compatible(&$Medium) {
 		$types = imageTypes();
 		if($types & IMG_GIF) {
-			$this->require['mimeTypes'][] = 'image/gif'; 
+			$this->require['mimeTypes'][] = 'image/gif';
 		}
 		if($types & IMG_JPG) {
 			$this->require['mimeTypes'][] = 'image/jpeg';
-		}		
+		}
 		if($types & IMG_PNG) {
 			$this->require['mimeTypes'][] = 'image/png';
 		}
 		if($types & IMG_WBMP) {
 			$this->require['mimeTypes'][] = 'image/wbmp';
-		}	
+		}
 		if($types & IMG_XPM) {
-			$this->require['mimeTypes'][] = 'image/xpm'; 
-		}		
+			$this->require['mimeTypes'][] = 'image/xpm';
+		}
 		return parent::compatible($Medium);
 	}
-	
+
 	function initialize(&$Medium) {
 		$this->_pngFilters = PNG_ALL_FILTERS;
 		$this->_format = $this->_formatMap[$Medium->mimeType]; // could be a problem here...
@@ -120,57 +120,57 @@ class GdMediumAdapter extends MediumAdapter {
 		if (!isset($Medium->file)) {
 			return false;
 		}
-		
-		$Medium->resources['gd'] = call_user_func_array('imageCreateFrom' . $this->_format, $Medium->file); 
-		 
+
+		$Medium->resources['gd'] = call_user_func_array('imageCreateFrom' . $this->_format, $Medium->file);
+
 		if (!$this->_isResource($Medium->resources['gd'])) {
 			return false;
 		}
-		
+
 		if (imageIsTrueColor($Medium->resources['gd'])) {
 			imageSaveAlpha($Medium->resources['gd'], true);
 		}
-		
+
 		return true;
 	}
-	
+
 	function toString(&$Medium) {
 		ob_start();
 		$this->store($Medium, null);
 		return ob_get_clean();
 	}
-	
+
 	function store(&$Medium, $file) {
 		if ($this->_format == 'jpeg') {
 			$args = array($Medium->resources['gd'], $file);
-			
+
 			if (isset($this->_jpegQuality)) {
 				$args[] = $this->_jpegQuality;
-			}			
+			}
 		} else if ($this->_format == 'png') {
 			$args = array($Medium->resources['gd'], $file);
-			
+
 			if (isset($this->_pngCompression)) {
 				$args[] = $this->_pngCompression;
-				
+
 				if (isset($this->_pngFilters)) {
 					$args[] = $this->_pngFilters;
 				}
 			}
 		} else {
 			$args = array($Medium->resources['gd'], $file);
-		}	
-		
+		}
+
 		return call_user_func_array('image' . $this->_format, $args);
 	}
-	
+
 	function convert(&$Medium, $mimeType) {
 		if (in_array($mimeType, $this->require['mimeTypes'])) {
 			return $this->_format = $this->_formatMap[$mimeType];
 		}
 		return false;
-	}	
-	
+	}
+
 	function crop(&$Medium, $left, $top, $width, $height) {
 		$left   = intval($left);
 		$top    = intval($top);
@@ -187,24 +187,24 @@ class GdMediumAdapter extends MediumAdapter {
 			imageCopyResampled($Image, $Medium->resources['gd'], 0, 0, $left, $top, $width, $height, $width, $height);
 		}
 		if ($this->_isResource($Image)) {
-			$this->_Image = $Image;
+			$Medium->resources['gd'] = $Image;
 			return true;
-		} 
+		}
 		return false;
-	}	
-	
+	}
+
 	function resize(&$Medium, $width, $height) {
 		$width  = intval($width);
 		$height = intval($height);
-		
+
 		$Image = imageCreateTrueColor($width, $height);
-		
+
 		if ($this->_isTransparent($Medium->resources['gd'])) {
 			$Image = $this->_copyTransparency($Medium->resources['gd'], $Image);
 			imageCopyResized($Image, $Medium->resources['gd'], 0, 0, 0, 0, $width, $height, $this->width($Medium), $this->height($Medium));
 		} else {
 			imageSaveAlpha($Image, true);
-			imageCopyResampled($Image, $Medium->resources['gd'], 0, 0, 0, 0, $width, $height, $this->width($Medium), $this->height($Medium));		
+			imageCopyResampled($Image, $Medium->resources['gd'], 0, 0, 0, 0, $width, $height, $this->width($Medium), $this->height($Medium));
 		}
 		if ($this->_isResource($Image)) {
 			$Medium->resources['gd'] = $Image;
@@ -212,7 +212,7 @@ class GdMediumAdapter extends MediumAdapter {
 		}
 		return false;
 	}
-	
+
 	function cropAndResize(&$Medium, $cropLeft, $cropTop, $cropWidth, $cropHeight, $resizeWidth, $resizeHeight) {
 		$cropLeft     = intval($cropLeft);
 		$cropTop      = intval($cropTop);
@@ -220,9 +220,9 @@ class GdMediumAdapter extends MediumAdapter {
 		$cropHeight   = intval($cropHeight);
 		$resizeWidth  = intval($resizeWidth);
 		$resizeHeight = intval($resizeHeight);
-		
+
 		$Image = imageCreateTrueColor($resizeWidth, $resizeHeight);
-		
+
 		if ($this->_isTransparent($Medium->resources['gd'])) {
 			$Image = $this->_copyTransparency($Medium->resources['gd'], $Image);
 			imageCopyResized($Image, $Medium->resources['gd'], 0, 0, $cropLeft, $cropTop, $resizeWidth, $resizeHeight, $cropWidth, $cropHeight);
@@ -233,26 +233,26 @@ class GdMediumAdapter extends MediumAdapter {
 		if ($this->_isResource($Image)) {
 			$Medium->resources['gd'] = $Image;
 			return true;
-		} 
-		return false;		
+		}
+		return false;
 	}
-	
+
 	function width(&$Medium) {
 		return imageSX($Medium->resources['gd']);
 	}
-	
+
 	function height(&$Medium) {
 		return imageSY($Medium->resources['gd']);
 	}
-	
+
 	function _isResource($Image) {
 		return is_resource($Image) && get_resource_type($Image) == 'gd';
 	}
-	
+
 	function _isTransparent($Image)	{
 		return imageColorTransparent($Image) >= 0;
 	}
-	
+
 	function _copyTransparency($sourceImage, $destinationImage)	{
 		$rgba  = imageColorsForIndex($sourceImage, imageColorTransparent($sourceImage));
 		$color = imageColorAllocate($destinationImage, $rgba['red'], $rgba['green'], $rgba['blue']);
