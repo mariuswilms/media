6d3f09db25bb9a09ca8817694c089e4f6c6ce386
David Persson <davidpersson@qeweurope.org>
2008-11-11 19:28:08 +0100

    Adding configuration for MimeType class

--- config/core.php
+++ config/core.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Configuration File
- * 
+ *
  * Configuration for the media plugin
- * 
+ *
  * Include it in your app's core.php or bootstrap.php then overwrite configuration
  * values if you like.
- * 
+ *
  * Copyright (c) $CopyrightYear$ David Persson
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -15,10 +15,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,10 +26,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE
- * 
+ *
  * PHP version $PHPVersion$
  * CakePHP version $CakePHPVersion$
- * 
+ *
  * @category   configuration
  * @package    attm
  * @subpackage attm.plugins.media.config
@@ -40,7 +40,7 @@
  * @version    Release: $Version$
  * @link       http://cakeforge.org/projects/attm The attm Project
  * @since      media plugin 0.50
- * 
+ *
  * @modifiedby   $LastChangedBy$
  * @lastmodified $Date$
  */
@@ -48,69 +48,82 @@
 	/**
 	 * An absolute (slash terminated) path to a directory holding media files
 	 * E.g.: /var/www/example.org/htdocs/app/webroot/media/
-	 * 
+	 *
 	 * Please also review the "directory layout" section in the docs
 	 */
-	if(!defined('MEDIA')) {
+	if (!defined('MEDIA')) {
 		define('MEDIA', WWW_ROOT . 'media' . DS);
 	}
 
 	/**
-	 * Either a complete URL or an path fragment relative to your webroot 
+	 * Either a complete URL or an path fragment relative to your webroot
 	 * (slash terminated)
-	 *   
+	 *
 	 * E.g.: http://www.example.org/app/media/
 	 * E.g.: media/
 	 */
-	if(!defined('MEDIA_URL')) {
+	if (!defined('MEDIA_URL')) {
 		define('MEDIA_URL', 'media/');
 	}
 
 	/**
+	 * Settings used by MimeType class
+	 */
+	Configure::write('Mime.glob', array(
+		'engine' => null, // null (auto detect) or core
+		'db' => null, // absolute path to a glob db file in freedesktop, apache, or php format
+	));
+
+	Configure::write('Mime.magic', array(
+		'engine' => null, // null (auto detect), core, fileinfo or mime_magic
+		'db' => null, // absolute path to a magic db file in freedesktop, apache, or php format
+	));
+
+	/**
 	 * Media filters
-	 * 
+	 *
 	 * A filter is a set of instructions
-	 * Each instruction represents a call to a method of the Medium class 
-	 */	
+	 * Each instruction represents a call to a method of the Medium class
+	 */
 	Configure::write('Media.filter.audio', array());
 
-	Configure::write('Media.filter.css',array(
+	Configure::write('Media.filter.css', array(
 		'c'		=> array('compress'),
 		)
-	);	
+	);
 
 	Configure::write('Media.filter.document', array(
-		'xxs'	=> array('convert' => 'image/png', 'zoomCrop' => array(16,16)),
-		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100,100)),
-		'm'		=> array('convert' => 'image/png', 'fit' => array(300,300)),
+		'xxs'	=> array('convert' => 'image/png', 'zoomCrop' => array(16, 16)),
+		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100, 100)),
+		'm'		=> array('convert' => 'image/png', 'fit' => array(300, 300)),
 		)
 	);
 
 	Configure::write('Media.filter.generic', array());
-	
-	Configure::write('Media.filter.image',array(
-		'xxs' 	=> array('convert' => 'image/png', 'zoomCrop' => array(16,16)),
-		'xs'	=> array('convert' => 'image/png', 'zoomCrop' => array(32,32)),
-		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100,100)),
-		'm'		=> array('convert' => 'image/png', 'fit' => array(300,300)),
-		'l'		=> array('convert' => 'image/png', 'fit' => array(450,450)),
-		'xl'	=> array('convert' => 'image/png', 'fit' => array(680,440)),
+
+	Configure::write('Media.filter.image', array(
+		'xxs' 	=> array('convert' => 'image/png', 'zoomCrop' => array(16, 16)),
+		'xs'	=> array('convert' => 'image/png', 'zoomCrop' => array(32, 32)),
+		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100, 100)),
+		'm'		=> array('convert' => 'image/png', 'fit' => array(300, 300)),
+		'l'		=> array('convert' => 'image/png', 'fit' => array(450, 450)),
+		'xl'	=> array('convert' => 'image/png', 'fit' => array(680, 440)),
 		)
 	);
-	
+
 	Configure::write('Media.filter.icon', array());
-	
-	Configure::write('Media.filter.js',array(
+
+	Configure::write('Media.filter.js', array(
 		'c'		=> array('compress'),
 		)
 	);
-	
-	Configure::write('Media.filter.text',array());
-	
-	Configure::write('Media.filter.video',array(
-		'xxs'	=> array('convert' => 'image/png', 'zoomCrop' => array(16,16)),
-		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100,100)),
-		'm'		=> array('convert' => 'image/png', 'fit' => array(300,300)),
+
+	Configure::write('Media.filter.text', array());
+
+	Configure::write('Media.filter.video', array(
+		'xxs'	=> array('convert' => 'image/png', 'zoomCrop' => array(16, 16)),
+		's'		=> array('convert' => 'image/png', 'fitCrop' => array(100, 100)),
+		'm'		=> array('convert' => 'image/png', 'fit' => array(300, 300)),
 		)
 	);
 ?>
