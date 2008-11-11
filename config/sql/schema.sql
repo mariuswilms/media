3b71c519c70e4eacfe8996b46d4511de6dd68212
David Persson <davidpersson@qeweurope.org>
2008-11-11 20:27:39 +0100

    Moving attachments schema into main schema file

--- config/sql/schema.sql
+++ config/sql/schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS `attachments` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `model` varchar(255) NOT NULL,
+  `foreign_key` int(10) NOT NULL,
+  `dirname` varchar(255) default NULL,
+  `basename` varchar(255) NOT NULL,
+  `checksum` varchar(255) NOT NULL,
+  `alternative` varchar(50) default NULL,
+  `group` varchar(255) default NULL,
+  `created` datetime default NULL,
+  `modified` datetime default NULL,
+  PRIMARY KEY  (`id`)
+);
+
 CREATE TABLE IF NOT EXISTS `media` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `dirname` varchar(255) default NULL,
@@ -8,3 +22,4 @@ CREATE TABLE IF NOT EXISTS `media` (
   `modified` datetime default NULL,
   PRIMARY KEY  (`id`)
 );
+
