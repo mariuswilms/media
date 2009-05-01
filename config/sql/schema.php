e9c5c29c5fc96cce8274e1577a2ec653bb7fe358
David Persson <davidpersson@qeweurope.org>
2009-05-01 16:28:11 +0200

    Removing media table from schema

--- config/sql/schema.php
+++ config/sql/schema.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Media Plugin Schema File
+ * Media Schema File
  *
  * Copyright (c) 2007-2009 David Persson
  *
@@ -17,13 +17,12 @@
  * @link       http://github.com/davidpersson/media
  */
 /**
- * Media Plugin Schema Class
+ * Media Schema Class
  *
  * @package    media
  * @subpackage media.config.sql
  */
-class MediaPluginSchema extends CakeSchema {
-	var $name = 'MediaPlugin';
+class MediaSchema extends CakeSchema {
 
 	function before($event = array()) {
 		return true;
@@ -36,18 +35,6 @@ class MediaPluginSchema extends CakeSchema {
 			'id'          => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'extra' => 'auto_increment', 'length' => 10),
 			'model'       => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
 			'foreign_key' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-			'dirname'     => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 255),
-			'basename'    => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
-			'checksum'    => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
-			'group'       => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 255),
-			'alternative' => array('type' => 'string', 'null' => true, 'default' => NULL,'length' => 50),
-			'created'     => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-			'modified'    => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-			'indexes'     => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
-			);
-
-	var $media = array(
-			'id'          => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'extra' => 'auto_increment', 'length' => 10),
 			'dirname'     => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 255),
 			'basename'    => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
 			'checksum'    => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
--- config/sql/schema.sql
+++ config/sql/schema.sql
@@ -12,13 +12,3 @@ CREATE TABLE IF NOT EXISTS `attachments` (
   PRIMARY KEY  (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `media` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `dirname` varchar(255) default NULL,
-  `basename` varchar(255) NOT NULL,
-  `checksum` varchar(255) NOT NULL,
-  `alternative` varchar(50) default NULL,
-  `created` datetime default NULL,
-  `modified` datetime default NULL,
-  PRIMARY KEY  (`id`)
-);
