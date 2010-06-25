52ec1424d93f23c3646da8e9aea99639f9ade391
David Persson <davidpersson@gmx.de>
2010-06-25 23:51:08 +0200

    Updating mm lib.

--- libs/mm/src/Media/Process/Adapter/Imagick.php
+++ libs/mm/src/Media/Process/Adapter/Imagick.php
@@ -41,7 +41,11 @@ class Media_Process_Adapter_Imagick extends Media_Process_Adapter {
 
 	public function __construct($handle) {
 		$this->_object = new Imagick();
-		$this->_object->readImageFile($handle);
+
+		// @fixme Workaaround for imagick failing to work with handles before module version 3.0.
+		// See http://pecl.php.net/bugs/bug.php?id=16932 for more information.
+		// $this->_object->readImageFile($handle);
+		$this->_object->readImageBlob(stream_get_contents($handle, -1, 0));
 
 		$mimeType = Mime_Type::guessType($handle);
 
