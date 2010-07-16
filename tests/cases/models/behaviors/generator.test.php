3016b831ba7ddd920e0f644c3e540af99bdcb59e
David Persson <davidpersson@gmx.de>
2010-07-16 14:18:03 +0200

    Addtional test and better isolation for generator behavior.

--- tests/cases/models/behaviors/generator.test.php
+++ tests/cases/models/behaviors/generator.test.php
@@ -26,36 +26,40 @@ require_once dirname(__FILE__) . DS . 'base.test.php';
  */
 class GeneratorBehaviorTestCase extends BaseBehaviorTestCase {
 
+	var $_backup;
+
 	function setUp() {
 		parent::setUp();
 		$this->_behaviorSettings = array(
 			'baseDirectory' => $this->Folder->pwd(),
-			'createDirectory' => false,
 			'filterDirectory' => $this->Folder->pwd() . 'filter' . DS
 		);
+		$this->_backup['configMedia'] = Configure::read('Media');
+	}
+
+	function tearDown() {
+		parent::tearDown();
+		Configure::write('Media', $this->_backup['configMedia']);
 	}
 
 	function testSetup() {
-		$Model =& ClassRegistry::init('TheVoid');
+		$Model = ClassRegistry::init('TheVoid');
 		$Model->Behaviors->attach('Media.Generator');
 
-		$Model =& ClassRegistry::init('Song');
+		$Model = ClassRegistry::init('Song');
 		$Model->Behaviors->attach('Media.Generator');
 	}
 
-	function testMakeVersion() {
+	function testMakeThroughModel() {
 		Configure::write('Media.filter.image', array(
 			's' => array('convert' => 'image/png', 'fit' => array(5, 5)),
 			'm' => array('convert' => 'image/png', 'fit' => array(10, 10))
 		));
 
-		$Model =& ClassRegistry::init('Unicorn', 'Model');
-
+		$Model = ClassRegistry::init('Unicorn', 'Model'); // has makeVersion mocked
 		$Model->Behaviors->attach('Media.Generator', array(
-			'baseDirectory' => $this->Folder->pwd(),
-			'filterDirectory' => $this->Folder->pwd() . 'filter' . DS,
 			'createDirectory' => true
-		));
+		) + $this->_behaviorSettings);
 
 		$file = $this->Data->getFile(array(
 			'image-jpg.jpg' => $this->Folder->pwd() . 'image-jpg.jpg'
@@ -78,5 +82,129 @@ class GeneratorBehaviorTestCase extends BaseBehaviorTestCase {
 		$Model->make($file);
 		$this->assertEqual($Model->makeVersionArgs, $expected);
 	}
+
+	function testCreateDirectory() {
+		Configure::write('Media.filter.image', array(
+			's' => array('convert' => 'image/png'),
+			'm' => array('convert' => 'image/png')
+		));
+
+		$Model = ClassRegistry::init('Unicorn', 'Model');
+		$Model->Behaviors->attach('Media.Generator', array(
+			'createDirectory' => false
+		) + $this->_behaviorSettings);
+
+		$file = $this->Data->getFile(array(
+			'image-jpg.jpg' => $this->Folder->pwd() . 'image-jpg.jpg'
+		));
+
+		$this->expectError();
+		$this->expectError();
+
+		$Model->make($file);
+
+		$this->assertFalse(is_dir($this->Folder->pwd() . 'filter' . DS . 's'));
+		$this->assertFalse(is_dir($this->Folder->pwd() . 'filter' . DS . 'm'));
+
+		$Model->Behaviors->attach('Media.Generator', array(
+			'createDirectory' => true
+		) +  $this->_behaviorSettings);
+
+		$Model->make($file);
+
+		$this->assertTrue(is_dir($this->Folder->pwd() . 'filter' . DS . 's'));
+		$this->assertTrue(is_dir($this->Folder->pwd() . 'filter' . DS . 'm'));
+	}
+
+	function testCreateDirectoryMode() {
+		Configure::write('Media.filter.image', array(
+			's' => array('convert' => 'image/png'),
+		));
+
+		$Model = ClassRegistry::init('Unicorn', 'Model');
+		$Model->Behaviors->attach('Media.Generator', array(
+			'createDirectoryMode' => 0755
+		) + $this->_behaviorSettings);
+
+		$file = $this->Data->getFile(array(
+			'image-jpg.jpg' => $this->Folder->pwd() . 'image-jpg.jpg'
+		));
+
+		$Model->make($file);
+		$this->assertEqual(decoct(fileperms($this->Folder->pwd() . 'filter' . DS . 's')), 40755);
+
+		$Model->Behaviors->attach('Media.Generator', array(
+			'createDirectoryMode' => 0777
+		) + $this->_behaviorSettings);
+
+		$Model->make($file);
+		$this->assertEqual(decoct(fileperms($this->Folder->pwd() . 'filter' . DS . 's')), 40755);
+
+		rmdir($this->Folder->pwd() . 'filter' . DS . 's');
+
+		$Model->make($file);
+		$this->assertEqual(decoct(fileperms($this->Folder->pwd() . 'filter' . DS . 's')), 40777);
+	}
+
+	function testMakeVersion() {
+		$config = Media_Process::config();
+
+		$message = '%s Need media processing adapter for image.';
+		$skipped = $this->skipIf(!isset($config['image']), $message);
+
+		if ($skipped) {
+			return;
+		}
+
+		$Model = ClassRegistry::init('Unicorn', 'Model');
+		$Model->Behaviors->attach('Media.Generator', $this->_behaviorSettings);
+
+		$file = $this->Data->getFile(array(
+			'image-jpg.jpg' => $this->Folder->pwd() . 'image-jpg.jpg'
+		));
+		$directory = $this->Folder->pwd() . 'filter' . DS . 's' . DS;
+		mkdir($directory);
+
+		$result = $Model->Behaviors->Generator->makeVersion($Model, $file, array(
+			'version' => 's',
+			'directory' => $directory,
+			'instructions' => array(
+				'convert' => 'image/png'
+			)
+		));
+		$this->assertTrue($result);
+		$this->assertTrue(file_exists($directory . 'image-jpg.png'));
+	}
+
+	function testMakeVersionAccrossMedia() {
+		$config = Media_Process::config();
+
+		$message = '%s Need media processing adapters configured for both image and document.';
+		$skipped = $this->skipIf(!isset($config['image'], $config['document']), $message);
+
+		if ($skipped) {
+			return;
+		}
+
+		$Model = ClassRegistry::init('Unicorn', 'Model');
+		$Model->Behaviors->attach('Media.Generator', $this->_behaviorSettings);
+
+		$file = $this->Data->getFile(array(
+			'application-pdf.pdf' => $this->Folder->pwd() . 'application-pdf.pdf'
+		));
+		$directory = $this->Folder->pwd() . 'filter' . DS . 's' . DS;
+		mkdir($directory);
+
+		$result = $Model->Behaviors->Generator->makeVersion($Model, $file, array(
+			'version' => 's',
+			'directory' => $directory,
+			'instructions' => array(
+				'convert' => 'image/png'
+			)
+		));
+		$this->assertTrue($result);
+		$this->assertTrue(file_exists($directory . 'application-pdf.png'));
+	}
 }
+
 ?>
