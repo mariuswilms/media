79ce198651b034341786689910e266acfbe6306a
David Persson <davidpersson@gmx.de>
2011-06-29 10:37:26 +0200

    Replacing usage of uses.

--- tests/fixtures/test_data.php
+++ tests/fixtures/test_data.php
@@ -16,7 +16,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link       http://github.com/davidpersson/media
  */
-uses('object');
+App::import('Core', 'Object');
 
 /**
  * Test Data Class
