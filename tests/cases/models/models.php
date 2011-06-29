1d69779116f8b86a9ca615b3b3fb2c30fb9d24a7
David Persson <davidpersson@gmx.de>
2011-06-29 10:51:51 +0200

    Removing name and useTable property from test models.

--- tests/cases/models/models.php
+++ tests/cases/models/models.php
@@ -1,18 +1,13 @@
 <?php
 class Movie extends CakeTestModel {
-	public $name = 'Movie';
-	public $useTable = 'movies';
 	public $hasMany = array('Actor');
 }
 
 class Actor extends CakeTestModel {
-	public $name = 'Actor';
-	public $useTable = 'actors';
 	public $belongsTo = array('Movie');
 }
 
 class Unicorn extends CakeTestModel {
-	public $name = 'Unicorn';
 	public $useTable = false;
 	public $makeVersionArgs = array();
 	public $returnMakeVersion = true;
@@ -24,6 +19,4 @@ class Unicorn extends CakeTestModel {
 }
 
 class Pirate extends CakeTestModel {
-	public $name = 'Pirate';
-	public $useTable = 'pirates';
 }
